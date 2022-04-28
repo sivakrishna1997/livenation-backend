1a24aab6090521fd6c1a0bf486ae5c1025cfe1b0
Siva Krishna <krishna88861@gmail.com>
2022-04-28 16:02:17 +0530

    event community package

--- src/modals_and_controls/packages/packages.schema.ts
+++ src/modals_and_controls/packages/packages.schema.ts
@@ -1,12 +1,12 @@
 import * as mongoose from 'mongoose';
 
 export const packages = mongoose.model('packages', new mongoose.Schema({
+    ticket_id: { type: String, required: true },
     title: { type: String, required: true, trim: true, index: true },
     price: { type: String },
     equivalent_points: { type: String },
     available_quantity: { type: String },
     inclusions: { type: String },
-
     cdate: { type: Date },
     udate: { type: Date }
 })
