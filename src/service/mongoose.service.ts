e99e8ae6002d3a94e5700d4c48aa2ee5ae65cf86
Siva Krishna <krishna88861@gmail.com>
2022-05-06 11:53:32 +0530

    ui change

--- src/service/mongoose.service.ts
+++ src/service/mongoose.service.ts
@@ -7,7 +7,7 @@ export default function mongodb() {
         useUnifiedTopology: true,
         useNewUrlParser: true
     }
-    // let url = "mongodb://localhost:27017/livenation";
+    // let url = "mongodb://localhost:27017/livenation"; 
     let url = "mongodb+srv://tal:tal@cluster0.jmvgw.mongodb.net/livenation";
     mongoose.connect(url, options).then(
         response => {
